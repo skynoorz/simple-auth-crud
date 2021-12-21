5bc4309e6a5060ac28dc7ca1727dce6f1d201b15
skynoorz <aligatorbol@gmail.com>
2021-12-21 16:19:45 -0400

    basic auth with interface

--- src/main/java/com/crud/bo/interviewtest/auth/SecurityConfig.java
+++ src/main/java/com/crud/bo/interviewtest/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.crud.bo.interviewtest.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,8 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().
-                authorizeRequests().antMatchers("/**")
-                .fullyAuthenticated().and().httpBasic();
+                authorizeRequests()
+                .antMatchers("/basicauth").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/users").hasAnyRole()
+                .antMatchers("/**").fullyAuthenticated()
+                .and().httpBasic();
     }
 
     //    REDUNDANT?
--- src/main/java/com/crud/bo/interviewtest/controllers/LoginController.java
+++ src/main/java/com/crud/bo/interviewtest/controllers/LoginController.java
@@ -1,14 +1,35 @@
 package com.crud.bo.interviewtest.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class LoginController {
-    @GetMapping("/")
-    public String login(){
-        return "authenticated succesfully";
+
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @GetMapping(path = "/basicauth")
+    public ResponseEntity<?> basicauth(@RequestHeader MultiValueMap<String, String> headers) {
+
+        headers.forEach((key, value) -> {
+            logger.info(String.format(
+                    "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Authenticated");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
